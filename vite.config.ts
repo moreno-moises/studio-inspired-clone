05e903726a120d991904d1704360b9efa8046503
moreno-moises <77939156+moreno-moises@users.noreply.github.com>
2026-04-23 16:31:28 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/studio-inspired-clone/",
   server: {
     host: "::",
     port: 8080,
